0147d7faa165d8bd49a2ad6f6217ea26526fbf24
Nicolas Arrastia <nicolasarrastia04@gmail.com>
2025-02-27 14:57:00 -0300

    change secure of cookie

--- Backend/AbsolutifyBack/controllers/spotifyController.js
+++ Backend/AbsolutifyBack/controllers/spotifyController.js
@@ -45,15 +45,15 @@ exports.callback = async (req, res) => {
     const { access_token, refresh_token } = response.data;
 
     res.cookie("spotifyAccessToken", access_token, {
-      httpOnly: false,
+      httpOnly: true,
       secure: true, // Cambiar a true en producción
-      sameSite: "Strict",
+      sameSite: "None",
       maxAge: 3600 * 1000, // 1 hora
     });
     res.cookie("spotifyRefreshToken", refresh_token, {
-      httpOnly: false,
+      httpOnly: true,
       secure: true, // Cambiar a true en producción
-      sameSite: "Strict",
+      sameSite: "None",
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 días
     });
 
@@ -91,9 +91,9 @@ exports.refresh = async (req, res) => {
     accessToken = newAccessToken;
     // Guardar el nuevo access token en cookies
     res.cookie("spotifyAccessToken", newAccessToken, {
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
-      sameSite: "Strict",
+      sameSite: "None",
       maxAge: 3600 * 1000, // 1 hora
     });
 
